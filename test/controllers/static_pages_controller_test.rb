d860ff9c56008ed2883824c16eae3d142b924ea6
sarah <clergentsarah@gmail.com>
2017-08-07 21:49:06 -0400

    fix test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -21,7 +21,7 @@ end
   test "should get help" do
     get help_path
     assert_response :success
-     assert_select "title", "#{@base_title}"
+     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
