9e152ebecde3d6851a0e7218e8b0513e6118f4dd
sarah <clergentsarah@gmail.com>
2017-08-13 16:56:50 -0400

    maj

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,53 @@
 class UsersController < ApplicationController
-  
-  def new
-    @user = User.new
-  end
+      before_action :logged_in_user, only: [:edit, :update]
 
-  def show 
-    @user = User.find(params[:id])
-  
-  end
-  
-  def create 
-    @user = User.new(user_params)
-    if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else 
-      render 'new'
+    def new
+        @user = User.new
     end
     
-  end
+    def show
+        @user = User.find(params[:id])
+    end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-  
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            log_in @user
+            flash[:success] = "Welcome to the Sample App!"
+            redirect_to @user
+        else
+            render 'new'
+        end
+    end
 
-  private
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update_attributes(user_params)
+            flash[:success] = "Profile updated"
+            redirect_to @user
+        else
+            render 'edit'
+        end
+    end
+
+    private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-end
 
+    # Before filters
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+end
