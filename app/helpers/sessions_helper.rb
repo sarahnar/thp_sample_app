a1f3ec06a775e957137aeca3e90f49bde4b279b6
sarah <clergentsarah@gmail.com>
2017-08-13 19:23:21 -0400

    finish user edit, update, add destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,23 +24,37 @@ module SessionsHelper
     end
 end
 
+    def current_user?(user)
+        user == current_user
+    end
 
     # Returns true if the user is logged in, false otherwise.
     def logged_in?
         !current_user.nil?
     end
 
- # Forgets a persistent session.
+    # Forgets a persistent session.
     def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
     end
 
     def log_out
-    forget(current_user)
-    session.delete(:user_id)
-    @current_user = nil
+        forget(current_user)
+        session.delete(:user_id)
+        @current_user = nil
+    end
+
+    # Redirects to stored location (or to the default).
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+
+  # Stores the URL trying to be accessed.
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
     end
 
 end
